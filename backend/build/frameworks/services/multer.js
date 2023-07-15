b2d6a16a1a0bd8bbd3213089b4bdae0dd18b3153
sujith888 <sujith.ss.zf@gmail.com>
2023-07-15 23:07:12 +0530

    ()=>final bug solved

--- backend/build/frameworks/services/multer.js
+++ backend/build/frameworks/services/multer.js
@@ -27,36 +27,3 @@ const interviewerStorage = multer_1.default.diskStorage({
 exports.interviewExpertUpload = (0, multer_1.default)({
     storage: interviewerStorage
 });
-// import multer from 'multer';
-// import cloudinary from 'cloudinary';
-// import { v2 as cloudinaryV2 } from 'cloudinary';
-// import { CloudinaryStorage } from 'multer-storage-cloudinary';
-// import { v4 as uuidv4 } from 'uuid';
-// import configKeys from '../../config'
-// // Configure Cloudinary
-// cloudinaryV2.config({ 
-//     cloud_name: configKeys.cloud_name,
-//     api_key: configKeys.api_key,
-//     api_secret: configKeys.api_secret 
-//   });
-// // Configure multer storage using Cloudinary
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinaryV2,
-//   params: {
-//     folder: (req: any, file: any) => 'uploads', // Specify the folder in Cloudinary where you want to store the files
-//     resource_type: (req: any, file: any) => {
-//       // Determine the resource type based on the file mimetype
-//       if (file.mimetype.startsWith('video/')) {
-//         return 'video';
-//       }
-//       return 'auto';
-//     },
-//     public_id: (req: Express.Request, file: Express.Multer.File) => {
-//       const fileName = `${uuidv4()}-${file.originalname}`;
-//       return fileName;
-//     }
-//   } as any
-// });
-// // Create multer instance with Cloudinary storage
-// const upload = multer({ storage });
-// export { upload };
