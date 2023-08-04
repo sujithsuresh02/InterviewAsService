feff255935978b9c04c0c5c222b7b870d46bad41
sujith888 <sujith.ss.zf@gmail.com>
2023-08-04 12:37:17 +0530

    cors bug fixed

--- backend/src/frameworks/webserver/express.ts
+++ backend/src/frameworks/webserver/express.ts
@@ -18,8 +18,7 @@ const expressConfig = (app: Application) => {
 
   app.use(
     cors({
-      // origin: ['https://www.interviewxperts.online','https://interviewxperts.online'],
-      origin: ['http://localhost:5173'],
+      origin: ['https://www.interviewxperts.online','https://interviewxperts.online'],
       methods: "GET, POST, PUT, PATCH, DELETE",
       exposedHeaders: ["Authorization", "X-My-Custom-Header"],
     })
